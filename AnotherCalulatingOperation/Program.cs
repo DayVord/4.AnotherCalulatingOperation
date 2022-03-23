7a42dc2e76ba2212598b05265c52454bf7fbb321
Dima Logvinets <mister.dayvord@gmail.com>
2022-03-23 19:36:25 +0300

    Добавьте файлы проекта.

---
+++ AnotherCalulatingOperation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AnotherCalulatingOperation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(158*(20+57));
+            Console.Read();
+        }
+    }
+}
